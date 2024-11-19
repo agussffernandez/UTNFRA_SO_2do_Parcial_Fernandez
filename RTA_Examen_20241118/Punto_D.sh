81da8d1ddbf69a2ccaa321ba8b66842b1e44f5de
Agustina <agustinabelenfernandez05@gmail.com>
2024-11-18 20:20:12 -0500

    ADD: Punto_D.sh y las modificaciones necesarias en el archivo 202406/ansible

---
+++ RTA_Examen_20241118/Punto_D.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Cambiar al directorio donde está el proyecto de Ansible
+cd /home/agustina/UTNFRA_SO_2do_Parcial_Fernandez/202406/ansible
+
+# Ejecutar el playbook de Ansible
+sudo ansible-playbook -i inventory/hosts playbook.yml
