488e01abdae021b1fcb02accebc9a1d10407b7b4
Agustina <agustinabelenfernandez05@gmail.com>
2024-11-18 21:01:37 -0500

    Agregando las modificaciones a  /202406/ansible y /202406/docker/

---
+++ 202406/docker/make-build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Haciendo build de la imagen
+docker build -t agussffernandez/web1-Fernandez:latest .
---
+++ 202406/docker/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Ejecutar el contenedor usando la imagen construida (crea la imagen luego del build)
+docker run -d -p 8080:80 agussffernandez/web1-Fernandez
